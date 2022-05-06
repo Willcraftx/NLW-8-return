751d0897a9c34ba48ae23784a5a7ab42624151bf
William.Y <100946502+Willcraftx@users.noreply.github.com>
2022-05-06 12:30:58 -0300

    newbie3

--- script.js
+++ script.js
@@ -6,5 +6,12 @@ function onScroll() {
   }
 }
 
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
   
 
